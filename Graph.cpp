a270b743236450527c101d246d9bd183ae145e2d
MalikSaqib141947 <37288377+MalikSaqib141947@users.noreply.github.com>
2019-06-01 06:34:02 +0500

    directed or undirected found

--- Graph.cpp
+++ Graph.cpp
@@ -5,6 +5,12 @@ struct Node{
 	char data;
 	Node *next;
 };
+
+struct edges{
+	char initial;
+	char terminal;
+	edges * next;
+};
 int degree(Node * vertex){
 	int count = -1;
 	while(vertex != NULL){
@@ -13,13 +19,48 @@ int degree(Node * vertex){
 	}
 	return count;
 }
-int Edges(Node * Graph[],int verteces){
-	int sum_of_degrees = 0;
-	for(int i = 0; i < verteces; i++){
-		sum_of_degrees = sum_of_degrees + degree(Graph[i]);
+int sum_of_degrees(Node * graph[], int ver){
+	int sum = 0;
+	for(int i = 0; i < ver; i++){
+		sum = sum + degree(graph[i]);
 	}
+	return sum;
+}
+bool edgeExists(edges * start, char a, char b){
+	edges * trav = start;
+	bool exists = false;
+	while(trav != NULL){
+		if(trav->initial == a && trav->terminal == b){
+			exists = true;
+			break;
+		}
+		trav = trav->next;
+	}
+	return exists;
+}
+bool is_directed(edges * e, Node * graph[], int vertices){
+	bool directed = false;
+	for(int i = 0; i<vertices; i++){
+		Node * start = graph[i];
+		Node * trav = graph[i];
+		while(trav->next != NULL){
+			if(edgeExists(e,trav->next->data,start->data)){
+				trav = trav->next;
+			}
+			else{
+				directed = true;
+				break;
+			}
+		}
+		if(! directed)
+			break;
+	}
+	return directed;
+}
+int Edges(Node * Graph[],int verteces){
 	
-	return sum_of_degrees/2;
+	int sum = sum_of_degrees(Graph,verteces);
+	return sum/2;
 }
 bool already_present(char array[], char ch, int len){
 	bool present = false;
@@ -37,12 +78,12 @@ bool taken(int array[], int x, int c){
 	}
 	return result;
 }
-bool connected(Node * Graph[], int ver ){
+bool connected(Node * Graph[], int ver, int start){
 	bool result = true;
 	int count = 0;
 	int count2 = 0;
 	char closure[ver];
-	closure[0] = Graph[0]->data;
+	closure[0] = Graph[start]->data;
 	int i = 0;
 	int added[ver];
 	while(i < ver){
@@ -81,6 +122,15 @@ int main(int argc, char** argv) {
 	cin >> vertices;
 	Node * Graph1[vertices];
 	Node * Graph2[vertices];
+	
+	edges * Eg1 = NULL;
+	edges * Eg2 = NULL;
+	
+	edges * start_Eg1 = Eg1;
+	edges * end_Eg1 = Eg1;
+	
+	edges * start_Eg2 = Eg1;
+	edges * end_Eg2 = Eg1;
 	//naming the vertices of Graph 1
 	cout << "Enter the names of the vertices of graph 1: " << endl;
 	for(int i = 0; i < vertices; i++){
@@ -103,11 +153,16 @@ int main(int argc, char** argv) {
 		Node * start_G1 = temp2;
 		Node * end_G1 = temp2;
 		end_G1->next = NULL;
-		cout << "Vertex " << Graph1[i]->data << " is connected to how many vertices?: ";
+		
+		cout << "Vertex " << Graph1[i]->data << " is initial to how many terminal vertices?: ";
 		int to_how_many;
 		cin >> to_how_many;
 		cout << "Enter the name of those vertices: " << endl;
 		for(int j = 0; j < to_how_many; j++){
+			
+			
+			
+			
 			Node *temp = new Node;
 			cout << "Vertex " << j+1 << ": ";
 			cin >> temp->data;
@@ -120,6 +175,22 @@ int main(int argc, char** argv) {
 				end_G1->next = temp;
 				end_G1 = temp;
 				end_G1->next = NULL;
+			}
+			
+			edges * e = new edges;
+			e->initial = Graph1[i]->data;
+			e->terminal = temp->data;
+			
+			if(start_Eg1 == NULL){
+				start_Eg1 = e;
+				end_Eg1 = e;
+				end_Eg1->next = NULL;
+			}
+			
+			else{
+				end_Eg1->next = e;
+				end_Eg1 = e;
+				end_Eg1->next = NULL;
 			}
 				
 		}
@@ -149,25 +220,71 @@ int main(int argc, char** argv) {
 				end_G2 = temp;
 				end_G2->next = NULL;
 			}
+			
+			edges * e2 = new edges;
+			e2->initial = Graph2[i]->data;
+			e2->terminal = temp->data;
+			
+			if(start_Eg2 == NULL){
+				start_Eg2 = e2;
+				end_Eg2 = e2;
+				end_Eg2->next = NULL;
+			}
+			
+			else{
+				end_Eg2->next = e2;
+				end_Eg2 = e2;
+				end_Eg2->next = NULL;
+			}
 				
 		}
 	}
+	
+	
 	cout << "*****************************************" << endl;
-	
 	//counting the edges of both the graphs
-	int edges1 = Edges(Graph1,vertices);
-	int edges2 = Edges(Graph2,vertices);
 	
+	int edges1;
+	int edges2;
+	if(! is_directed(start_Eg1,Graph1,vertices))
+		edges1 = Edges(Graph1,vertices);
+	else
+		edges1 = sum_of_degrees(Graph1,vertices);
+		
+	if(! is_directed(start_Eg2,Graph2,vertices))
+		edges2 = Edges(Graph2,vertices);
+	else 
+		edges2 = sum_of_degrees(Graph2,vertices);
+		
 	if(edges1 == edges2)
 		cout << "Graph 1 and Graph 2 contain equal  " << edges1 << " edges!" << endl;
 	else
 		cout << "Graph 1 and Graph 2 dont contain equal edges!" << endl;
 	
-	if(connected(Graph2,vertices) && connected(Graph1,vertices) ){
+	//if(connected(Graph2,vertices) && connected(Graph1,vertices) ){
+	//	cout << "The Graphs are connected!" << endl;
+	//}
+	
+	bool conn = true;
+	for(int i = 0; i < vertices; i++){
+		if(! connected(Graph1,vertices,i)){
+			cout << "The Graphs are not connected!" << endl;
+			conn = false;
+			break;
+		}	
+	}
+	if(conn)
+	for(int i = 0; i < vertices; i++){
+		if(! connected(Graph2,vertices,i)){
+			cout << "The Graphs are not connected!" << endl;
+			conn = false;
+			break;
+		}	
+	}
+	
+	if(conn){
 		cout << "The Graphs are connected!" << endl;
 	}
-	else
-	 	cout << "The Graphs are not connected!" << endl;
 	
 	return 0;
 }
