065dea7585621cd82b4cd7f157c3a729cd33f90b
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2019-06-02 10:50:18 +0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -14,7 +14,11 @@ struct edges{
 	edges * next;
 };
 
-int degree(Node * vertex){
+//Fnction declaration
+void degreeCount(Node* graph1[], Node* graph2[] , int verticesG1, int verticesG2);
+
+//Function returns the degree count of the vertex
+int degree (Node * vertex) {
 	int count = -1;
 	while(vertex != NULL){
 		count ++;
@@ -23,19 +27,21 @@ int degree(Node * vertex){
 	return count;
 }
 
-int sum_of_degrees(Node * graph[], int ver){
+//Function returns the sum of degrees
+int sum_of_degrees (Node * graph[], int ver) {
 	int sum = 0;
-	for(int i = 0; i < ver; i++){
+	for(int i = 0; i < ver; i++) {
 		sum = sum + degree(graph[i]);
 	}
 	return sum;
 }
 
-bool edgeExists(edges * start, char a, char b){
+//Function checks whether an edge exists between two points or not
+bool edgeExists (edges * start, char a, char b) {
 	edges * trav = start;
 	bool exists = false;
-	while(trav != NULL){
-		if(trav->initial == a && trav->terminal == b){
+	while(trav != NULL) {
+		if(trav->initial == a && trav->terminal == b) {
 			exists = true;
 			break;
 		}
@@ -44,33 +50,37 @@ bool edgeExists(edges * start, char a, char b){
 	return exists;
 }
 
-bool is_directed(edges * e, Node * graph[], int vertices){
+//Function checks whether the graph is directed or not
+bool is_directed (edges * e, Node * graph[], int vertices) {
 	bool directed = false;
-	for(int i = 0; i<vertices; i++){
+	for (int i = 0; i < vertices; i++) {
 		Node * start = graph[i];
 		Node * trav = graph[i];
-		while(trav->next != NULL){
-			if(edgeExists(e,trav->next->data,start->data)){
+		while (trav->next != NULL) {
+			//Check for undirected
+			if (edgeExists(e, trav->next->data, start->data)) {
 				trav = trav->next;
 			}
-			else{
+			else {
 				directed = true;
 				break;
 			}
 		}
-		if(! directed)
+		if (! directed)
 			break;
 	}
 	return directed;
 }
 
-int Edges(Node * Graph[],int verteces){
+//Function return the number of edges in the graph
+int Edges (Node * Graph[], int vertices) {
 
-	int sum = sum_of_degrees(Graph,verteces);
+	int sum = sum_of_degrees(Graph, vertices);
 	return sum/2;
 }
 
-bool already_present(char array[], char ch, int len){
+
+bool already_present (char array[], char ch, int len) {
 	bool present = false;
 	for(int i = 0; i < len; i++){
 		if(array[i] == ch)
@@ -79,16 +89,16 @@ bool already_present(char array[], char ch, int len){
 	return present;
 }
 
-bool taken(int array[], int x, int c){
+bool taken(int array[], int x, int c) {
 	bool result = false;
-	for(int i = 0; i < c; i++){
-		if(x == array[i])
+	for (int i = 0; i < c; i++) {
+		if (x == array[i])
 			result = true;
 	}
 	return result;
 }
 
-bool connected(Node * Graph[], int ver, int start){
+bool connected(Node * Graph[], int ver, int start) {
 	bool result = true;
 	int count = 0;
 	int count2 = 0;
@@ -96,22 +106,23 @@ bool connected(Node * Graph[], int ver, int start){
 	closure[0] = Graph[start]->data;
 	int i = 0;
 	int added[ver];
-	while(i < ver){
+	while (i < ver) {
 		Node * trav = NULL;
-		for(int j = 0; j < ver; j++){
-			if(already_present(closure,Graph[j]->data,count+1) && !taken(added,j,count2)){
+		for (int j = 0; j < ver; j++) {
+			if (already_present(closure,Graph[j]->data,count+1) && !taken(added,j,count2)) {
 				added[count2] = j;
 				count2++;
 				trav = Graph[j];
+				break;
 			}
 		}
-		if(trav == NULL && count < ver){
+		if (trav == NULL && count < ver) {
 			result = false;
 			break;
 		}
 		else
-		while(trav != NULL){
-			if(!already_present(closure,trav->data,count+1)){
+		while(trav != NULL) {
+			if(!already_present(closure,trav->data,count+1)) {
 				count++;
 				closure[count] = trav->data;
 			}
@@ -119,7 +130,7 @@ bool connected(Node * Graph[], int ver, int start){
 		}
 		i++;
 	}
-	if(count == ver-1){
+	if(count == ver-1) {
 		result = true;
 	}
 
@@ -131,7 +142,7 @@ int main(int argc, char** argv) {
   //Declaration area
 	ifstream inFile;
 	string x; //Variable to receive input from the file
-  int counter;
+  int counter, verticesG1, verticesG2;
 
 	//Working area
   counter = 0;
@@ -147,11 +158,11 @@ int main(int argc, char** argv) {
 		cout << "\nError: Unable to open the file Vertices G1.txt\n";
 		exit(1);
 	}
-
+	//Close the file
   inFile.close();
-	int vertices = counter;
-	Node * Graph1[vertices];
 
+	verticesG1 = counter;
+	Node * Graph1[verticesG1];
 
 	edges * Eg1 = NULL;
 	edges * Eg2 = NULL;
@@ -161,16 +172,15 @@ int main(int argc, char** argv) {
 
 	edges * start_Eg2 = Eg1;
 	edges * end_Eg2 = Eg1;
+
 	//naming the vertices of Graph 1
-	cout << "Enter the names of the vertices of graph 1: " << endl;
   //Opening the file
   inFile.open("C:\\Users\\mtalh\\source\\repos\\Data Structure Project\\Vertices G1.txt");
   if (inFile) {
     //Loop through the file contents
-    for(int i = 0; i < vertices; i++){
+    for(int i = 0; i < verticesG1; i++){
       inFile >> x;
   		Graph1[i] = new Node;
-  		cout << "Vertex " << i+1 << ": ";
   		Graph1[i]->data = x[0];
   	}
   }
@@ -178,13 +188,14 @@ int main(int argc, char** argv) {
     cout << "\nError: Unable to open the file Vertices.txt\n";
 		exit(1);
   }
+	//Close the file
   inFile.close();
 
   //Open the vertices file
   inFile.open("C:\\Users\\mtalh\\source\\repos\\Data Structure Project\\Vertices G1.txt");
-  //Getting the vertices connected to each of the vertices of Graph 1
-	cout << "Graph 1:" << endl;
-	for(int i = 0; i < vertices; i++){
+
+	//Getting the vertices connected to each of the vertices of Graph 1
+	for(int i = 0; i < verticesG1; i++) {
 		Node * temp2 = Graph1[i];
 		Node * start_G1 = temp2;
 		Node * end_G1 = temp2;
@@ -231,7 +242,6 @@ int main(int argc, char** argv) {
   inFile.close();
 
 	//naming the vertices of graph 2
-
   //Open the file
   inFile.open("C:\\Users\\mtalh\\source\\repos\\Data Structure Project\\Vertices G2.txt");
 
@@ -249,20 +259,19 @@ int main(int argc, char** argv) {
   //Close the file
   inFile.close();
 
-  vertices = counter;
+  verticesG2 = counter;
 
   //Declare the graph
-  Node * Graph2[vertices];
+  Node * Graph2[verticesG2];
 
   //Open the file
   inFile.open("C:\\Users\\mtalh\\source\\repos\\Data Structure Project\\Vertices G2.txt");
 
   if (inFile) {
     //Loop through the contents and store root vertices
-    for(int i = 0; i < vertices; i++){
+    for(int i = 0; i < verticesG2; i++){
       inFile >> x;
   		Graph2[i] = new Node;
-  		cout << "Vertex " << i+1 << ": ";
   		Graph2[i]->data = x[0];
   	}
   }
@@ -276,8 +285,7 @@ int main(int argc, char** argv) {
   inFile.open("C:\\Users\\mtalh\\source\\repos\\Data Structure Project\\Vertices G2.txt");
 
 	//Getting the vertices connected to each of the vertices of Graph 2
-	cout << "Graph2: " << endl;
-	for(int i = 0; i < vertices; i++){
+	for(int i = 0; i < verticesG2; i++){
   	Node * temp1 = Graph2[i];
   	Node * start_G2 = temp1;
   	Node * end_G2 = temp1;
@@ -328,15 +336,15 @@ int main(int argc, char** argv) {
 
 	int edges1;
 	int edges2;
-	if(! is_directed(start_Eg1,Graph1,vertices))
-		edges1 = Edges(Graph1,vertices);
+	if(! is_directed(start_Eg1,Graph1,verticesG1))
+		edges1 = Edges(Graph1,verticesG1);
 	else
-		edges1 = sum_of_degrees(Graph1,vertices);
+		edges1 = sum_of_degrees(Graph1,verticesG1);
 
-	if(! is_directed(start_Eg2,Graph2,vertices))
-		edges2 = Edges(Graph2,vertices);
+	if(! is_directed(start_Eg2,Graph2,verticesG2))
+		edges2 = Edges(Graph2,verticesG2);
 	else
-		edges2 = sum_of_degrees(Graph2,vertices);
+		edges2 = sum_of_degrees(Graph2,verticesG2);
 
 	if(edges1 == edges2)
 		cout << "Graph 1 and Graph 2 contain equal  " << edges1 << " edges!" << endl;
@@ -347,17 +355,17 @@ int main(int argc, char** argv) {
 	//	cout << "The Graphs are connected!" << endl;
 	//}
 
-	bool conn = true;
-	for(int i = 0; i < vertices; i++){
-		if(! connected(Graph1,vertices,i)){
+	bool conn = true; //Boolean variable holding connected status
+	for(int i = 0; i < verticesG1; i++){
+		if(! connected(Graph1,verticesG1,i)){
 			cout << "The Graphs are not connected!" << endl;
 			conn = false;
 			break;
 		}
 	}
 	if(conn)
-	for(int i = 0; i < vertices; i++){
-		if(! connected(Graph2,vertices,i)){
+	for(int i = 0; i < verticesG2; i++){
+		if(! connected(Graph2, verticesG2, i)){
 			cout << "The Graphs are not connected!" << endl;
 			conn = false;
 			break;
@@ -368,5 +376,22 @@ int main(int argc, char** argv) {
 		cout << "The Graphs are connected!" << endl;
 	}
 
+	degreeCount(Graph1, Graph2, verticesG1, verticesG2);
+
 	return 0;
+}
+void degreeCount(Node* graph1[], Node* graph2[] , int verticesG1, int verticesG2) {
+	//Declaration area
+
+	//Working area
+	cout << "\nGraph 1\n";
+	for (int i = 0; i < verticesG1; i++) {
+		cout << "Degree of " << graph1[i]->data  << " is: "<< degree(graph1[i]) << endl;
+	}
+
+	cout << "\nGraph 2\n";
+	for (int i = 0; i < verticesG2; i++) {
+		cout << "Degree of " << graph2[i]->data  << " is: "<< degree(graph2[i]) << endl;
+	}
+	//cout << graph1[0]->data << endl;
 }
